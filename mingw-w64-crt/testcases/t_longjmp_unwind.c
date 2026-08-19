39dfdec5062fa7ee588c2bc7cae646cdbe934036
Pali Rohár <pali.rohar@gmail.com>
2026-08-19 21:44:14 +0200

    crt: test: Add new t_longjmp_unwind test which verifies that unwind handler is called from longjmp exactly once

---
+++ mingw-w64-crt/testcases/t_longjmp_unwind.c
@@ -0,0 +1,233 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <setjmp.h>
+#include <excpt.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+/* mingw-w64 headers */
+#include "libtest.h"
+
+#if defined(__SEH__) && (!defined(__clang__) || __clang_major__ >= 7)
+#define SEH_INLINE_ASM
+#ifdef __arm__
+#define ASM_SEH_UNWIND "%%unwind"
+#elif defined(__aarch64__)
+#define ASM_SEH_UNWIND "@unwind, @except"
+#else
+#define ASM_SEH_UNWIND "@unwind"
+#endif
+#endif
+
+static int outer_unwind_called = 0;
+static int target_unwind_called = 0;
+static int inner_unwind_called = 0;
+static jmp_buf buf;
+
+#if defined(__i386__)
+/* We need to make sure that we align the stack to 16 bytes for the sake of SSE */
+__attribute__((force_align_arg_pointer))
+#endif
+static EXCEPTION_DISPOSITION __cdecl outer_unwind_handler(EXCEPTION_RECORD *ExceptionRecord, PVOID EstablisherFrame __attribute__((unused)), CONTEXT *ContextRecord __attribute__((unused)), PVOID DispatcherContext __attribute__((unused)))
+{
+  printf("System called outer SEH unwind handler with flags 0x%lx\n", ExceptionRecord->ExceptionFlags);
+  if (ExceptionRecord->ExceptionFlags & EXCEPTION_UNWINDING)
+    outer_unwind_called++;
+  return ExceptionContinueSearch;
+}
+
+#if defined(__i386__)
+/* We need to make sure that we align the stack to 16 bytes for the sake of SSE */
+__attribute__((force_align_arg_pointer))
+#endif
+static EXCEPTION_DISPOSITION __cdecl target_unwind_handler(EXCEPTION_RECORD *ExceptionRecord, PVOID EstablisherFrame __attribute__((unused)), CONTEXT *ContextRecord __attribute__((unused)), PVOID DispatcherContext __attribute__((unused)))
+{
+  printf("System called target SEH unwind handler with flags 0x%lx\n", ExceptionRecord->ExceptionFlags);
+  if ((ExceptionRecord->ExceptionFlags & (EXCEPTION_UNWINDING | EXCEPTION_TARGET_UNWIND)) == (EXCEPTION_UNWINDING | EXCEPTION_TARGET_UNWIND))
+    target_unwind_called++;
+  return ExceptionContinueSearch;
+}
+
+#if defined(__i386__)
+/* We need to make sure that we align the stack to 16 bytes for the sake of SSE */
+__attribute__((force_align_arg_pointer))
+#endif
+static EXCEPTION_DISPOSITION __cdecl inner_unwind_handler(EXCEPTION_RECORD *ExceptionRecord, PVOID EstablisherFrame __attribute__((unused)), CONTEXT *ContextRecord __attribute__((unused)), PVOID DispatcherContext __attribute__((unused)))
+{
+  printf("System called inner SEH unwind handler with flags 0x%lx\n", ExceptionRecord->ExceptionFlags);
+  if (ExceptionRecord->ExceptionFlags & EXCEPTION_UNWINDING)
+    inner_unwind_called++;
+  return ExceptionContinueSearch;
+}
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+#endif
+#if !defined(__i386__) && defined(SEH_INLINE_ASM)
+__attribute__((noinline)) /* required for asm .seh_handler directive */
+#endif
+#if defined(__i386__)
+__attribute__((noinline)) /* prevents optimizing out exception_record structure before calling longjmp() */
+#endif
+static void register_inner_unwind_handler_and_call_longjmp(void)
+{
+#if defined(__i386__)
+  EXCEPTION_REGISTRATION_RECORD inner_exception_record = {
+    .Next = (EXCEPTION_REGISTRATION_RECORD *)__readfsdword(0),
+    .Handler = (PEXCEPTION_ROUTINE)(INT_PTR)inner_unwind_handler,
+  };
+  __writefsdword(0, (DWORD)&inner_exception_record);
+#elif defined(SEH_INLINE_ASM)
+  asm volatile (".seh_handler %c0, " ASM_SEH_UNWIND :: "i" (inner_unwind_handler));
+#else
+  return; /* should not happen; triggers runtime error */
+#endif
+
+  printf("Calling longjmp() and unregistering inner SEH unwind handler\n");
+  longjmp(buf, 1);
+
+  /* NOTE: longjmp does not return and on i386 it unregister all SEH handlers up to the target one */
+
+  printf("Unregistering inner SEH unwind handler\n");
+#if defined(__i386__)
+  if (__readfsdword(0) != (DWORD)&inner_exception_record) {
+    printf("ERROR: Cannot unregister inner SEH unwind handler because of corrupted list of SEH records\n");
+    _exit(1);
+  }
+  __writefsdword(0, (DWORD)inner_exception_record.Next);
+#endif
+}
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+#if !defined(__i386__) && defined(SEH_INLINE_ASM)
+__attribute__((noinline)) /* required for asm .seh_handler directive */
+#endif
+static void register_target_unwind_handler_and_call_setjmp_and_register_inner_unwind_handler_and_call_longjmp(void)
+{
+#if defined(__i386__)
+  EXCEPTION_REGISTRATION_RECORD target_exception_record = {
+    .Next = (EXCEPTION_REGISTRATION_RECORD *)__readfsdword(0),
+    .Handler = (PEXCEPTION_ROUTINE)(INT_PTR)target_unwind_handler,
+  };
+  __writefsdword(0, (DWORD)&target_exception_record);
+#elif defined(SEH_INLINE_ASM)
+  asm volatile (".seh_handler %c0, " ASM_SEH_UNWIND :: "i" (target_unwind_handler));
+#else
+  return; /* should not happen; triggers runtime error */
+#endif
+
+  printf("Calling setjmp()\n");
+
+  if (!setjmp(buf)) {
+    printf("Registering inner SEH unwind handler\n");
+    register_inner_unwind_handler_and_call_longjmp();
+    printf("ERROR: longjmp() returned\n");
+    _exit(1);
+  }
+
+  printf("setjmp() returned from longjmp()\n");
+
+  printf("Unregistering target SEH unwind handler\n");
+#if defined(__i386__)
+  if (__readfsdword(0) != (DWORD)&target_exception_record) {
+    printf("ERROR: Cannot unregister target SEH unwind handler because of corrupted list of SEH records\n");
+    _exit(1);
+  }
+  __writefsdword(0, (DWORD)target_exception_record.Next);
+#endif
+}
+
+#if !defined(__i386__) && defined(SEH_INLINE_ASM)
+__attribute__((noinline)) /* required for asm .seh_handler directive */
+#endif
+static void register_outer_unwind_handler_and_register_target_unwind_handler_and_call_setjmp_and_register_inner_unwind_handler_and_call_longjmp(void)
+{
+#if defined(__i386__)
+  EXCEPTION_REGISTRATION_RECORD outer_exception_record = {
+    .Next = (EXCEPTION_REGISTRATION_RECORD *)__readfsdword(0),
+    .Handler = (PEXCEPTION_ROUTINE)(INT_PTR)outer_unwind_handler,
+  };
+  __writefsdword(0, (DWORD)&outer_exception_record);
+#elif defined(SEH_INLINE_ASM)
+  asm volatile (".seh_handler %c0, " ASM_SEH_UNWIND :: "i" (outer_unwind_handler));
+#else
+  return; /* should not happen; triggers runtime error */
+#endif
+
+  printf("Registering target SEH unwind handler\n");
+  register_target_unwind_handler_and_call_setjmp_and_register_inner_unwind_handler_and_call_longjmp();
+
+  printf("Unregistering outer SEH unwind handler\n");
+#if defined(__i386__)
+  if (__readfsdword(0) != (DWORD)&outer_exception_record) {
+    printf("ERROR: Cannot unregister outer SEH unwind handler because of corrupted list of SEH records\n");
+    _exit(1);
+  }
+  __writefsdword(0, (DWORD)outer_exception_record.Next);
+#endif
+}
+
+int main(int argc, char **argv)
+{
+  mingw_test_init();
+
+#if !defined(__i386__) && !defined(SEH_INLINE_ASM)
+  printf("SKIP: No SEH support\n");
+  return 77;
+#endif
+
+  outer_unwind_called = 0;
+  target_unwind_called = 0;
+  inner_unwind_called = 0;
+
+  printf("Registering outer SEH unwind handler\n");
+  register_outer_unwind_handler_and_register_target_unwind_handler_and_call_setjmp_and_register_inner_unwind_handler_and_call_longjmp();
+
+  if (outer_unwind_called != 0) {
+    printf("ERROR: Outer SEH unwind handler was called\n");
+    return 1;
+  }
+
+  printf("OK: Outer SEH unwind handler was not called\n");
+
+#ifdef __i386__
+  /* i386 fs:0 based SEH does not call the last (target) handler */
+  if (target_unwind_called != 0) {
+    printf("ERROR: Target SEH unwind handler was called\n");
+    return 1;
+  }
+
+  printf("OK: Target SEH unwind handler was not called\n");
+#else
+  /* non-i386 .pdata based SEH always calls the last target handler with the EXCEPTION_TARGET_UNWIND flag */
+  if (target_unwind_called == 0) {
+    printf("ERROR: Target SEH unwind handler was not called\n");
+    return 1;
+  }
+
+  if (target_unwind_called != 1) {
+    printf("ERROR: Target SEH unwind handler was called more times (%d)\n", target_unwind_called);
+    return 1;
+  }
+
+  printf("OK: Target SEH unwind handler was called exactly once\n");
+#endif
+
+  if (inner_unwind_called == 0) {
+    printf("ERROR: Inner SEH unwind handler was not called\n");
+    return 1;
+  }
+
+  if (inner_unwind_called != 1) {
+    printf("ERROR: Inner SEH unwind handler was called more times (%d)\n", inner_unwind_called);
+    return 1;
+  }
+
+  printf("OK: Inner SEH unwind handler was called exactly once\n");
+
+  return 0;
+}
