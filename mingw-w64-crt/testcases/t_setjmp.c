ca9cf0adfaed22f8e7c133de5c038325f65e4be9
Pali Rohár <pali.rohar@gmail.com>
2026-08-23 20:59:52 +0200

    crt: test: Extend t_setjmp test with different longjmp values

--- mingw-w64-crt/testcases/t_setjmp.c
+++ mingw-w64-crt/testcases/t_setjmp.c
@@ -6,26 +6,40 @@
 
 jmp_buf buf;
 
-void __attribute__((noreturn)) foo (void)
+void __attribute__((noreturn)) call_longjmp (int val)
 {
-  printf ("Call longjmp\n");
-  longjmp (buf, 1);
+  printf ("Call longjmp with %d\n", val);
+  longjmp (buf, val);
 }
 
 int main ()
 {
   mingw_test_init ();
 
-  if (setjmp (buf))
+  for (volatile int in_val = -300; in_val <= 300; in_val++)
     {
-      printf ("Caught\n");
-    }
-  else
-    {
-      foo ();
-      printf ("Should not be reached.\n");
-      return 1;
+      printf ("Call setjmp\n");
+      volatile int in_longjmp = 0;
+      int out_val = setjmp (buf);
+      if ((in_val != 0 && out_val == in_val) || (in_val == 0 && out_val == 1)) /* longjmp (0) cause that setjmp () returns 1 */
+        {
+          printf ("Caught %d\n", out_val);
+        }
+      else if (!in_longjmp && out_val == 0)
+        {
+          in_longjmp = 1;
+          call_longjmp (in_val);
+          printf ("Should not be reached.\n");
+          return 1;
+        }
+      else
+        {
+          printf ("Unexpected value %d from %s\n", out_val, in_longjmp ? "longjmp" : "setjmp");
+          return 1;
+        }
+      printf ("\n");
     }
 
+  printf ("SUCCESS\n");
   return 0;
 }
