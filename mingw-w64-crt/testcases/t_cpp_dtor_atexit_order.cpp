75eee64efb14a0e40ef64230c5a406b4246213a3
Pali Rohár <pali.rohar@gmail.com>
2026-09-06 00:40:37 +0200

    crt: test: Add new t_cpp_dtor_atexit_order test which verifies order of atexit callbacks and C++ thread local destructors

---
+++ mingw-w64-crt/testcases/t_cpp_dtor_atexit_order.cpp
@@ -0,0 +1,47 @@
+#include <cstdio>
+#include <cstdlib>
+
+/* mingw-w64 headers */
+#ifdef __MINGW64_VERSION_MAJOR
+#include "libtest.h"
+#endif
+
+template <void (*callback)()> struct dtor_callback { ~dtor_callback() { callback(); } };
+
+bool main_success = false;
+
+void cpp_tls_dtor_func() {
+    std::puts("C++ tls dtor func called");
+    if (!main_success) {
+        std::_Exit(1);
+    }
+    std::puts("TEST PASSED");
+    std::_Exit(0);
+}
+template struct dtor_callback<&cpp_tls_dtor_func>;
+thread_local dtor_callback<&cpp_tls_dtor_func> register_tls_dtor_callback;
+
+void atexit_func() {
+    std::puts("atexit func called");
+    std::puts("TEST FAILED");
+    std::_Exit(1);
+}
+
+int main() {
+#ifdef __MINGW64_VERSION_MAJOR
+    mingw_test_init();
+#endif
+
+    std::puts("registering atexit func");
+    std::atexit(atexit_func);
+
+    std::puts("referencing C++ TLS obj");
+    (void)register_tls_dtor_callback;
+
+    std::puts("registering atexit func");
+    std::atexit(atexit_func);
+
+    std::puts("returning from main");
+    main_success = true;
+    return 1;
+}
