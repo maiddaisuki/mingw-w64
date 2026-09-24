a24efc432f1d8826fae533d9936d24a75f7e29f0
Kirill Makurin <maiddaisuki@outlook.com>
2026-09-24 15:06:33 +0900

    crt: Implement _setjmpex for arm32 UCRT builds
    Function _setjmpex is available in arm32 msvcr110.dll, msvcr120.dll,
    msvcr120_app.dll and in msvcrt.dll DLL libs. It is also available in
    arm32 vcruntime140.dll DLL library which msvc toolchain uses for linking
    UCRT applications.
    
    For some unknown reasons this function is not available in arm32
    api-ms-win-crt-private-l1-1-0.dll and ucrtbase.dll libraries which
    mingw-w64 uses for UCRT builds. But it is available in arm64 versions of
    those libraries.
    
    When application includes setjmpex.h header file, the _setjmpex symbol is
    emitted for setjmp() calls. So without the _setjmpex symbol in libucrt.a
    library, the final linking for UCRT builds on arm32 is failing.
    
    As the mingw-w64 do not want to link to vcruntime140.dll runtime library,
    provide simple _setjmpex implementation for arm32 UCRT builds. This
    implementation will just call _setjmp symbol and manually stores the frame
    pointer (second function argument) which is the only difference between
    those two _setjmp and _setjmpex functions on arm32.

---
+++ mingw-w64-crt/misc/ucrt_arm32__setjmpex.c
@@ -0,0 +1,38 @@
+/**
+ * This file has no copyright assigned and is placed in the Public Domain.
+ * This file is part of the mingw-w64 runtime package.
+ * No warranty is given; refer to the file DISCLAIMER.PD within this package.
+ */
+
+#if defined(__arm__)
+
+#include <setjmp.h>
+
+/* arm32 _setjmp() function ignores it second frame argument and always stores
+ * the NULL pointer into Frame member of _JUMP_BUFFER structure.
+ * Implement our _setjmpex() function as a wrapper around the _setjmp() function
+ * and manually stores the frame pointer into the Frame member.
+ */
+int __cdecl __attribute__((naked)) __attribute__((nothrow, returns_twice)) _setjmpex(jmp_buf buf, void *frame)
+{
+    asm(
+        "push.w {r4, r5, lr}\n\t"
+        "mov    r4, r0\n\t"
+        "mov    r5, r1\n\t"
+        "bl     _setjmp\n\t"
+        "mov    r2, r4\n\t"
+        "mov    r1, r5\n\t"
+        "pop.w  {r4, r5, lr}\n\t"
+        "str    r1, [r2, #0x00]\n\t" /* jmp_buf->Frame */
+        "str    sp, [r2, #0x24]\n\t" /* jmp_buf->Sp */
+        "str    lr, [r2, #0x28]\n\t" /* jmp_buf->Pc */
+        "bx     lr\n\t"
+    );
+}
+int (__cdecl *__MINGW_IMP_SYMBOL(_setjmpex))(jmp_buf buf, void *frame) = _setjmpex;
+
+/* In UCRT is the _setjmpex() function aliased to __intrinsic_setjmpex() function. */
+int __attribute__((alias("_setjmpex"))) __cdecl __intrinsic_setjmpex(jmp_buf buf, void *frame);
+extern int __attribute__((alias(__MINGW64_STRINGIFY(__MINGW_IMP_SYMBOL(_setjmpex)))))(__cdecl *__MINGW_IMP_SYMBOL(__intrinsic_setjmpex))(jmp_buf buf, void *frame);
+
+#endif
